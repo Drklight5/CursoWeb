77ebbb31827d12bd459d5f303193c613153f8e02
Sarah Smith <ga27pro@gmail.com>
2022-02-19 18:49:30 -0600

    pipipapuche

--- Gold/Casas/Prueba.js
+++ Gold/Casas/Prueba.js
@@ -1,50 +1,54 @@
-var kk = false;
-var edad;
-var nombre;
 var numero = 0;
-
-if(kk){
-    ppga = promptInt(prompt("Escribe tu edad"));
-    ez = prompt("tu nombre")
-    alert("hola");
-    prompt("hola");
-}
-var calvox = {
+var calvox ={
     tipo: "normal",
     peso: "80kg",
-    vida: "10",
-    velocidad: "0.1", 
+    vida: 10,
+    velocidad: 0.1,
     comer:function(){
         console.log(" ricoche ");
     },
     daño:function(daño){
         calvox.vida = calvox.vida - daño;
+        if(calvox. vida <= 0){
+            calvox.muere();
+        }
+    },
+    muere:function(){
+        console.log("El calvox murio ");
+        document.getElementById("resultado").innerHTML = "El zombie ha muerto te ha costado " + numero + " intentos";
+    }
+}    
+var zombiechiquito = Object.create(calvox);
+zombiechiquito.vida = 1
+zombiechiquito.tamaño = "2cm";
+
+var zombiegun = Object.create(calvox);
+zombiegun.vida = 12 
+zombiegun.damage = 1000
+
+var zombieplanta = {
+    daño: -1,
+    ataque: function(){
+        calvox.daño(zombieplanta.daño);
     }
 }
+
 var lanzapapa =  {
-    daño = 5
+    daño: 3,
+    lanzapapa: 0,
+    ataque: function(){
+        numero = numero + 1;
+        calvox.daño(lanzapapa.daño);
+        
+}
 }
 var sandia = {
-    daño: 10
+    daño: 10,
+    ataque: function(){
+          numero = numero + 1;
+        calvox.daño(sandia.daño);
+      
 }
-calvox.daño(1);
-calvox.comer();
-console.log("El zombie que te ataco es de tipo: " + calvox.tipo);
-console.log("El zombie tiene una fuerza de: " + calvox.peso);
-console.log("le quedo " + calvox.vida + " de vida a calvox");
-
-console.error("Estas Feo");
-console.warn("casi lo echas a perder");
-
-llover();
-llover();
-llover();
-llover();
-
-console.log(numero);
-
-function llover(){
-    numero = numero + 1;
-
-    console.log("esta llovindo")
 }
+document.getElementById("title").innerHTML = "mata al zombie";
+document.getElementById("description").innerHTML  = "El zombie tiene " + calvox.vida + " de vida";
